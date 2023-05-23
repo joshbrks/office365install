7a857f3f1d18b1c0e8f2f0338a9574982dcd9cd8
Josh Westbrook <124754531+joshkwestbrook@users.noreply.github.com>
2023-05-23 09:23:33 -0500

    reorder of install

--- UninstallAllOfficeVersionsAndReinstallwithO365.ps1
+++ UninstallAllOfficeVersionsAndReinstallwithO365.ps1
@@ -564,6 +564,8 @@ if (Test-Path -Path 'C:\Support') {
 } else {
     New-Item -Path 'C:\Support\' -ItemType Directory
 }
+#Uninstall Built-In OneNote
+Get-AppxPackage *OneNote* | Remove-AppxPackage
 
 #Copy Installer
 Invoke-WebRequest -uri "https://c2rsetup.officeapps.live.com/c2r/download.aspx?productReleaseID=O365ProPlusRetail&platform=Def&language=en-us&TaxRegion=pr&correlationId=3dda7fc1-09ce-43fe-a9e3-8e6b7ea16401&token=0642b10e-77b5-44d4-a9df-982c54186524&version=O16GA&source=O15OLSO365&Br=2" -OutFile C:\Support\OfficeSetup.exe
@@ -588,8 +590,6 @@ Set-Content C:\support\OfficeConfig.xml @"
 "@
 Set-Location C:\Support\
 Start-Process .\OfficeSetup.exe -Wait -ArgumentList '/configure OfficeConfig.xml'
-#Uninstall Built-In OneNote
-Get-AppxPackage *OneNote* | Remove-AppxPackage
 }
 else
 {
